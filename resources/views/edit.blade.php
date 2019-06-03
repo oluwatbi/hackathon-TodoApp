fd16d16f41dff2a2252cf1732fdbdfe98736c6a2
Oyelowo Oluwatobiloba <oluwatobilobaoyelowo@gmail.com>
2019-06-03 21:44:07 +0100

    romoved unnesessary lines

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -1,25 +1,16 @@
 @extends('layouts.app')
-
 @section('content')
     <div class="container" xmlns="http://www.w3.org/1999/html">
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-
-
                     <div class="card-header"><h3>Edit Todolist</h3></div>
-
-
-                    <p>You are about to edit "<em>{{ $task->title }}</em></p>
-                    <form method="patch">
+                    <form method="post">
                         @csrf
-
                         <div class="form-group">
-                            <textarea name="title" class="form-control"  rows="3" placeholder=""></textarea>
+                            <textarea name="title" class="form-control"  rows="3" >{{ $task->title }}</textarea>
                         </div>
-                        <a href="/task/update/{{ $task->id }}">
                             <button type="submit" class="btn btn-outline-primary">Update</button>
-                        </a>
                     </form>
                 </div>
             </div>
